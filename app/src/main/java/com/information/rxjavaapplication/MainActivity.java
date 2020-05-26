779874ec445d9be479430b00cd165cfdd2c6812d
Jeong Gwanjin <jeong.gwanjin@revamp.co.jp>
2020-05-26 21:41:48 +0900

    RxAndroid공부시작

--- app/src/main/java/com/information/rxjavaapplication/MainActivity.java
+++ app/src/main/java/com/information/rxjavaapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.information.rxjavaapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,9 +9,11 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent = new Intent(getApplicationContext(), HelloActivity.class);
+                startActivity(intent);
+                return false;
             }
         });
     }
