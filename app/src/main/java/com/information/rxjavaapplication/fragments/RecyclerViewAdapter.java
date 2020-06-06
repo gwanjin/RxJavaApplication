5edbb4acabcfd54ff75ad4e5eda1d620d3b630fb
Jeong Gwanjin <jeong.gwanjin@revamp.co.jp>
2020-06-06 15:24:12 +0900

    2020/6/3 단말내패키지리스트표시(설명)

--- app/src/main/java/com/information/rxjavaapplication/fragments/RecyclerViewAdapter.java
+++ app/src/main/java/com/information/rxjavaapplication/fragments/RecyclerViewAdapter.java
@@ -27,23 +27,50 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         this.mPublishSubject = PublishSubject.create();
     }
 
+
+    /**
+     * onCreateViewHolder에서 ViewHolder 객체를 생성
+     * getItemCount()호출 후, Adapter의 onCreateViewHolder(ViewGroup, int) 메서드를 호출한다.
+     * onCreateviewHolder가 실행되면 RecyclerView는 Adapter에게 ViewHolder 객체를 받음.
+     * @param parent
+     * @param viewType
+     * @return ViewHolder 리턴
+     */
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // 직접 정의한 ViewHolder을 리턴
+        // 경우에 따라 한번만 호출
         final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_item, parent, false);
         return new MyViewHolder(view);
     }
 
+    /**
+     *
+     * onBindViewholder(ViewHolder, int)를 호출하면서 전에 Adapter에게 받았던 ViewHolder 객체와 리스트에서 해당 ViewHolder의 위치를 인자로 전달한다
+     * Adapter는 인자로 받은 위치에 맞는 데이터를 찾은 후 ViewHolder의 View에 결합
+     * @param holder ：Adapter에게 받았던 ViewHolder 객체
+     * @param position ：리스트에서 해당 ViewHolder의 위치
+     */
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        // RecyclerView에서 받은 위치 값을 토대로 알맞은 데이터를 찾은 후, holder인자의 뷰 아이템에 값을 넣어줌
         final RecyclerItem item = mItems.get(position);
         holder.mImageView.setImageDrawable(item.getImage());
         holder.mTitleView.setText(item.getTitle());
+        // 실시간으로 처리되어야 하기때문에 PublishSubject로 구현
         holder.getClickObserver(item).subscribe(mPublishSubject);
     }
 
+    /**
+     *
+     * 먼저 RecyclerView는 우리가 구현할 Adapter의 getItemCount() 메서드를 호출해서 RecyclerView에서 보여줘야하는 리스트 항목 총 갯수를 요청
+     *
+    * */
     @Override
     public int getItemCount() {
+        // DataSet을 직접 구현
+        // 저장된 배열 요소 개수 리턴
         return mItems.size();
     }
 
@@ -59,19 +86,31 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return mPublishSubject;
     }
 
+    // ViewHolder는 리스트 항목 하나의 View를 만들고 보존
     class MyViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.item_image)
         ImageView mImageView;
         @BindView(R.id.item_title)
         TextView mTitleView;
 
+        /**
+         *
+         * @param itemView ：리스트 항목에 대한 레이아웃
+         */
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
 
         Observable<RecyclerItem> getClickObserver(RecyclerItem item) {
+            // Click시 RecyclerItem(클릭한 아이템)을 발행
             return Observable.create(e -> itemView.setOnClickListener(view -> e.onNext(item)));
+//            return Observable.create(emitter -> itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    emitter.onNext(item);
+//                }
+//            }));
         }
     }
 }
--- app/src/main/java/com/information/rxjavaapplication/fragments/RecyclerViewFragment.java
+++ app/src/main/java/com/information/rxjavaapplication/fragments/RecyclerViewFragment.java
@@ -29,7 +29,8 @@ public class RecyclerViewFragment extends Fragment {
 
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
-
+    // RecyclerView는 단지 RecyclerView 항목들을 재활용하고 보여주는 역할만
+    // Adapter는 필요한 ViewHolder 객체를 생성하고 데이터를 ViewHolder 객체와 결합하는 역할
     private RecyclerViewAdapter mRecyclerViewAdapter;
     private Unbinder mUnbinder;
 
@@ -45,11 +46,16 @@ public class RecyclerViewFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        // RecyclerView는 직접 화면에 리스트 항목들을 위치시키진 않고, LayoutManager에게 해당 작업을 위임
+        // LayoutManager는 리스트 항복 View들을 화면에 위치시키고 스크롤 동작을 처리
         final LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(layoutManager);
 
         mRecyclerViewAdapter = new RecyclerViewAdapter();
         mRecyclerView.setAdapter(mRecyclerViewAdapter);
+        // PublishSubject는 클릭이벤트를 구독중 -> Click 이벤트가 RecyclerItem 발행
+        // publishSubject는 RecyclerItem를 발행
+        // PublishSubject<RecyclerItem> -> RecyclerItem객체리턴
         mRecyclerViewAdapter.getItemPublishSubject().subscribe(s -> toast(s.getTitle()));
     }
 
@@ -61,10 +67,14 @@ public class RecyclerViewFragment extends Fragment {
             return;
         }
 
+        // 패키지 목록 표시
         getItemObservable()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(item -> {
                     mRecyclerViewAdapter.updateItems(item);
+                    // 리스트 항목 뷰에 들어간 데이터가 바꼈을 때 호출
+                    // DataSet이 변경된 경우 그 내용을 등록된 Observer에 전달
+                    // LayoutManager는 모든 자룔르 다시 바인딩하고 모든 View를 다시 레이아웃하게됨
                     mRecyclerViewAdapter.notifyDataSetChanged();
                 });
     }
@@ -86,6 +96,7 @@ public class RecyclerViewFragment extends Fragment {
         Intent i = new Intent(Intent.ACTION_MAIN, null);
         i.addCategory(Intent.CATEGORY_LAUNCHER);
 
+        // 패키지 목록들을 RecyclerItem으로 순서대로 발행
         return Observable.fromIterable(pm.queryIntentActivities(i, 0))
                 .sorted(new ResolveInfo.DisplayNameComparator(pm))
                 .subscribeOn(Schedulers.io())
